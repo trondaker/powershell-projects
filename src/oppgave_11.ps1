c28cfb005cb7de71928652fdecfab0ef417ca678
trondaker <trond.aker@nav.no>
2021-11-29 13:12:30 +0100

    Changed oppgave_11.ps1 as try/catch didnt work when running as a function

--- src/oppgave_11.ps1
+++ src/oppgave_11.ps1
@@ -11,28 +11,17 @@
 2021-11-08T09:26:34.719 [Information] Executed 'Functions.Blackjack-aker' (Succeeded, Id=8078f41f-378d-4b61-bab9-9f4da8ff16e7, Duration=21335ms)
 #>
 
-# I stedet for å bruke Write-Ouput concater jeg $streng, og sender denne til slutt
-# som respons i http-responsen.
-
 ## The $TriggerMetadata parameter contains information about the triggered function. The sys property includes data like the date and time it is triggered, the HTTP method used to trigger it, and a unique GUID for the function's execution.
 param($Request, $TriggerMetadata)
 
 # Convert the incoming HTTP request body from JSON to a PowerShell object. The incoming HTTP request carries information about the request in the body. This information is accessible inside the function using the $Request parameter.
-$requestBodyObject = $Request.Body | ConvertFrom-Json
+#$requestBodyObject = $Request.Body | ConvertFrom-Json
 
-#param ($UrlKortstokk)
+# Dra inn kortstokken via en weblient. Try/catch funka ikke når oppgave_11.ps1 kjøres via Azure Functions (?)
+$web_client = New-Object Net.WebClient
+$kortstokk = $web_client.DownloadString("http://nav-deckofcards.herokuapp.com/shuffle") | ConvertFrom-Json
+$fikkKortstokk = $true
 
-# Dra inn kortstokken via en weblient.
-$web_client = new-object system.net.webclient
-try {
-    $kortstokk = $web_client.DownloadString("http://nav-deckofcards.herokuapp.com/shuffle") | ConvertFrom-Json
-    $fikkKortstokk = $true
-}
-# Hvis hostnamet ikke eksisterer e.l, sett fikkkortstokk til false og print melding.
-catch [System.Management.Automation.MethodInvocationException] {
-    "Nodename nor servname provided, or not known"
-    $fikkKortstokk = $false
-}
 #Ny funksjon som bare summerer verdiene til korta, slik at outputen blir helt riktig. Unngår Poengsum for hver kjøring.
 function summerVerdier {
     [OutputType([int])]
@@ -95,68 +84,69 @@ function kortstokkTilStreng {
 if ($fikkKortstokk) {
     $streng = $streng + "Kortstokk: $(kortstokkTilStreng -kortstokk $kortstokk)"
     $streng = $streng + "`nPoengsum:$(summerVerdier -kortstokk $kortstokk)"
-}
 
-# Tildeler meg selv 2 kort først.
-$meg = $kortstokk[0..1]
-# Fjerner de to første verdiene. -1 kan være 
-$kortstokk = $kortstokk[2..$kortstokk.Count]
-# Magnus får to kort.
-$magnus = $kortstokk[0..1]
-# Fjerner Magnus sine kort.
-$kortstokk = $kortstokk[2..$kortstokk.Count]
-# Skriver ut hvilke kort vi har og poengsum.
-$streng = $streng +  "`nmeg: $(kortstokkTilStreng -kortstokk $meg)"
-$streng = $streng +  "`nmagnus: $(kortstokkTilStreng -kortstokk $magnus)"
-# Printer ut kortstokken til slutt for å verifisere at korta er borte.
-$streng = $streng +  "`nKortstokk: $(kortstokkTilStreng -kortstokk $kortstokk)"
-# Gjenbruker summerVerdier-funksjonen for å regne ut verdiene på våre hender.
-$min_hand = $(summerVerdier -kortstokk $meg)
-$min_hand
-$magnus_hand = $(summerVerdier -kortstokk $magnus)
-# Hvis verdiene returnert for meg eller Magnus er 21, skriv ut at Blackjack.
-
-# While-løkke som trekker nye kort helt til min verdi er større enn 17.
-While ($min_hand -lt 17) {
-    $streng = $streng +  "`nJeg trekker kort, fortsatt under 17."
-    # Trekker nytt kort og fjerner kortet fra $kortstokk.
-    $meg = $meg + $kortstokk[0]
-    $kortstokk = $kortstokk[1..$kortstokk.Count]
-    # Summerer verdiene og kjører ny vurdering.
+    # Tildeler meg selv 2 kort først.
+    $meg = $kortstokk[0..1]
+    # Fjerner de to første verdiene. -1 kan være 
+    $kortstokk = $kortstokk[2..$kortstokk.Count]
+    # Magnus får to kort.
+    $magnus = $kortstokk[0..1]
+    # Fjerner Magnus sine kort.
+    $kortstokk = $kortstokk[2..$kortstokk.Count]
+    # Skriver ut hvilke kort vi har og poengsum.
+    $streng = $streng +  "`nmeg: $(kortstokkTilStreng -kortstokk $meg)"
+    $streng = $streng +  "`nmagnus: $(kortstokkTilStreng -kortstokk $magnus)"
+    # Printer ut kortstokken til slutt for å verifisere at korta er borte.
+    $streng = $streng +  "`nKortstokk: $(kortstokkTilStreng -kortstokk $kortstokk)"
+    # Gjenbruker summerVerdier-funksjonen for å regne ut verdiene på våre hender.
     $min_hand = $(summerVerdier -kortstokk $meg)
-} 
-
-While ($magnus_hand -lt 17) {
-    $streng = $streng +  "`nMagnus trekker kort, fortsatt under 17."
-    # Trekker nytt kort og fjerner kortet fra $kortstokk.
-    $magnus = $magnus + $kortstokk[0]
-    $kortstokk = $kortstokk[1..$kortstokk.Count]
-    # Summerer verdiene og kjører ny vurdering.
+    $min_hand
     $magnus_hand = $(summerVerdier -kortstokk $magnus)
-} 
+    # Hvis verdiene returnert for meg eller Magnus er 21, skriv ut at Blackjack.
 
-# $min_hand er nå over 17, sjekker hvor vi er og avslutter.
-if (($min_hand -gt 21) -and ($magnus_hand -gt 21)) {
-    $streng = $streng +  "`nBegge over 21, begge busted."
-} 
-if (($min_hand -eq 21) -and ($magnus_hand -eq 21)) {
-    $streng = $streng +  "`n`nBegge har blackjack!"
-} 
-if ($min_hand -eq 21) {
-    $streng = $streng +  "`nJeg har blackjack, vinner denne runden!"
+    # While-løkke som trekker nye kort helt til min verdi er større enn 17.
+    While ($min_hand -lt 17) {
+        $streng = $streng +  "`nJeg trekker kort, fortsatt under 17."
+        # Trekker nytt kort og fjerner kortet fra $kortstokk.
+        $meg = $meg + $kortstokk[0]
+        $kortstokk = $kortstokk[1..$kortstokk.Count]
+        # Summerer verdiene og kjører ny vurdering.
+        $min_hand = $(summerVerdier -kortstokk $meg)
+    } 
+
+    While ($magnus_hand -lt 17) {
+        $streng = $streng +  "`nMagnus trekker kort, fortsatt under 17."
+        # Trekker nytt kort og fjerner kortet fra $kortstokk.
+        $magnus = $magnus + $kortstokk[0]
+        $kortstokk = $kortstokk[1..$kortstokk.Count]
+        # Summerer verdiene og kjører ny vurdering.
+        $magnus_hand = $(summerVerdier -kortstokk $magnus)
+    } 
+
+    # $min_hand er nå over 17, sjekker hvor vi er og avslutter.
+    if (($min_hand -gt 21) -and ($magnus_hand -gt 21)) {
+        $streng = $streng +  "`nBegge over 21, begge busted."
+    } 
+    if (($min_hand -eq 21) -and ($magnus_hand -eq 21)) {
+        $streng = $streng +  "`n`nBegge har blackjack!"
+    } 
+    if ($min_hand -eq 21) {
+        $streng = $streng +  "`nJeg har blackjack, vinner denne runden!"
+    }
+    if ($magnus_hand -eq 21) {
+        $streng = $streng +  "`nMagnus har blackjack, vinner denne runden!"
+    }
+    if ( ($magnus_hand -lt 21) -and ($magnus_hand -gt $min_hand) ) {
+        $streng = $streng +  "`nIngen fikk blackjack, men Magnus kom nærmere 21 enn meg, han vant!"
+    }
+    if ( ($min_hand -lt 21) -and ($min_hand -gt $magnus_hand) ) {
+        $streng = $streng +  "`nIngen fikk blackjack, men jeg kom nærmere 21 enn Magnus, jeg vant!"
+    }
 }
-if ($magnus_hand -eq 21) {
-    $streng = $streng +  "`nMagnus har blackjack, vinner denne runden!"
-}
-if ( ($magnus_hand -lt 21) -and ($magnus_hand -gt $min_hand) ) {
-    $streng = $streng +  "`nIngen fikk blackjack, men Magnus kom nærmere 21 enn meg, han vant!"
-}
-if ( ($min_hand -lt 21) -and ($min_hand -gt $magnus_hand) ) {
-    $streng = $streng +  "`nIngen fikk blackjack, men jeg kom nærmere 21 enn Magnus, jeg vant!"
-}
+
 
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
+    StatusCode = [System.Net.HttpStatusCode]::OK
     Body = $streng
 })
 
